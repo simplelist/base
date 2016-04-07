f9891078afee9108f22b94d1a47f3046b2fa32da
罗曼蒂克S <707069921@qq.com>
2016-04-07 17:39:01 +0800

    爬虫 Gecco

--- src/com/jack/spider/jsoup/ColorBall.java
+++ src/com/jack/spider/jsoup/ColorBall.java
@@ -13,24 +13,5 @@ import java.util.List;
  * Created by jackshi on 16/3/19.
  */
 public class ColorBall {
-    public static void main(String[] args){
-        fetchContent(5);
-    }
-    public static List<String> fetchContent(int totalPage) {
-        Document document;
-        List<String> aList=new ArrayList<String>();
-        try {
-            for (int i = 1; i <= totalPage; i++) {
-                document = Jsoup.connect("http://www.qiushibaike.com/textnew/page/"+i+"/")
-                        .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36").get();
-                Elements elements = document.select(".content");
-                for (Element element : elements) {
-                    aList.add(element.text());
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return aList;
-    }
+
 }
